28adaa8d8e2ab7e019fe071c0b9bd66d49163ac9
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-04-26 13:12:03 +0800

    feat user Table

--- internal/repo/models/user.go
+++ internal/repo/models/user.go
@@ -1,6 +1,40 @@
 package models
 
+const (
+	TableNameUser = "t_users"
+)
+
+var UserColumn = &userColumn{
+	ID:        "id",
+	Account:   "account",
+	NickName:  "nick_name",
+	Password:  "password",
+	Age:       "age",
+	Sex:       "sex",
+	CreatedAt: "created_at",
+	UpdatedAt: "updated_at",
+	DeletedAt: "deleted_at",
+}
+
 type User struct {
 	BaseModel
-	Age uint64 `gorm:"column:age"`
+	Account  string `gorm:"column:account;type:varchar(50);not null;uniqueIndex:uniq_account;comment:账号" json:"account"` // 账号
+	NickName string `gorm:"column:nick_name;type:varchar(255);not null;comment:昵称" json:"nick_name"`                     // 昵称
+	Password string `gorm:"column:password;type:varchar(255);not null;comment:密码" json:"password"`                       // 密码
+	Age      uint8  `gorm:"column:age;type:tinyint unsigned;default:18;not null;comment:年龄" json:"age"`                  // 年龄
+	Sex      string `gorm:"column:sex;type:enum('0','1');not null;comment:性别" json:"sex"`                                // 性别
+}
+
+func (*User) TableName() string { return TableNameUser }
+
+type userColumn struct {
+	ID        string
+	Account   string
+	NickName  string
+	Password  string
+	Age       string
+	Sex       string
+	CreatedAt string
+	UpdatedAt string
+	DeletedAt string
 }

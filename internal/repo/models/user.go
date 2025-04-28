75fdf1eecd05ee4820e38a409dd15f82b124e62f
IScream915 <67994672+IScream915@users.noreply.github.com>
2025-04-28 16:49:28 +0800

    fix user model

--- internal/repo/models/user.go
+++ internal/repo/models/user.go
@@ -7,7 +7,7 @@ const (
 var UserColumn = &userColumn{
 	ID:        "id",
 	Account:   "account",
-	NickName:  "nick_name",
+	NickName:  "nickname",
 	Password:  "password",
 	Age:       "age",
 	Sex:       "sex",
@@ -19,9 +19,9 @@ var UserColumn = &userColumn{
 type User struct {
 	BaseModel
 	Account  string `gorm:"column:account;type:varchar(50);not null;uniqueIndex:uniq_account;comment:账号" json:"account"` // 账号
-	NickName string `gorm:"column:nick_name;type:varchar(255);not null;comment:昵称" json:"nick_name"`                     // 昵称
+	NickName string `gorm:"column:nickname;type:varchar(255);not null;comment:昵称" json:"nick_name"`                      // 昵称
 	Password string `gorm:"column:password;type:varchar(255);not null;comment:密码" json:"password"`                       // 密码
-	Age      uint8  `gorm:"column:age;type:tinyint unsigned;default:18;not null;comment:年龄" json:"age"`                  // 年龄
+	Age      uint8  `gorm:"column:age;type:int unsigned;default:18;not null;comment:年龄" json:"age"`                      // 年龄
 	Sex      string `gorm:"column:sex;type:enum('0','1');not null;comment:性别" json:"sex"`                                // 性别
 }
 
